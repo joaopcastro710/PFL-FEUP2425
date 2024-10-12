386db35bf8768124cf9880f0410f196a85e7e555
João Castro <up202206575@up.pt>
2024-10-12 22:20:40 +0100

    Update Aula3

--- Fichas/aula3.hs
+++ Fichas/aula3.hs
@@ -15,3 +15,60 @@ map f (filter p xs)
 -----------------------------------------
 
 --3.2
+
+dec2int :: [Int] -> Int
+dec2int = foldl (\acc x -> acc * 10 + x) 0
+
+-----------------------------------------
+
+--3.3
+
+zipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith _ [] _ = []
+zipWith _ _ [] = []
+zipWith f (x:xs) (y:ys) = f x y : zipWith f xs ys
+
+------------------------------------------
+
+--3.4
+
+isort :: Ord a => [a] -> [a]
+isort = foldr insert []
+
+------------------------------------------
+
+--3.7
+--(a)
+(++) :: [a] -> [a] -> [a]
+xs ++ ys = foldr (:) ys xs
+
+--(b)
+concat :: [[a]] -> [a]
+concat = foldr (++) []
+
+--(c)
+reverse :: [a] -> [a]
+reverse = foldr (\x acc -> acc ++ [x]) []
+
+--(d)
+reverse :: [a] -> [a]
+reverse = foldl (flip (:)) []
+
+--(e)
+elem :: Eq a => a -> [a] -> Bool
+elem x = any (== x)
+
+
+-------------------------------------
+
+--3.8
+--(a)
+palavras :: String -> [String]
+palavras [] = []
+palavras s = let (w, rest) = break (== ' ') (dropWhile (== ' ') s)
+             in w : palavras (dropWhile (== ' ') rest)
+
+
+--(b)
+despalavras :: [String] -> String
+despalavras = foldr1 (\w s -> w ++ " " ++ s)
